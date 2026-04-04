eb2a697d917f87f95dcc706866add004508bcb99
Zohra Hussaini <zhussaini306@gmail.com>
2026-04-03 22:41:27 -0500

    Update simon_game.py
    Added functions to generate and play a color sequence.

--- simon-says-game/simon_game.py
+++ simon-says-game/simon_game.py
@@ -3,3 +3,16 @@ import time
 import csv
 
 COLORS = ["red", "blue", "green", "yellow"]
+
+def generate_sequence(level):
+    sequence = []
+    for i in range(level):
+        sequence.append(random.choice(COLORS))
+    return sequence
+
+def play_sequence(sequence):
+    print("\nWatch the sequence:")
+    for color in sequence:
+        print(color)
+        time.sleep(0.8)
+    print("\n" * 20)
