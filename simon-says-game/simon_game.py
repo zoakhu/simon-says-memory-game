b9460368ea9e32a2e509f94f771ec3c6bfc62876
Zohra Hussaini <zhussaini306@gmail.com>
2026-04-03 22:52:13 -0500

    Update simon_game.py
    Add functions to get user input and check input validity

--- simon-says-game/simon_game.py
+++ simon-says-game/simon_game.py
@@ -16,3 +16,15 @@ def play_sequence(sequence):
         print(color)
         time.sleep(0.8)
     print("\n" * 20)
+
+def get_user_input(length):
+    user = input("enter the sequence (seperate by space): ")
+    return user.lower().split()
+
+def check_player_input(sequence, user_input):
+    if sequence == user_input:
+        return True
+    else:
+        return False 
+
+
